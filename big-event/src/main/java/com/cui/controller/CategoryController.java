f7dc0c0d187bde72e40ecdbb68e9f01647ec4987
xuyucheng <norbocampasjb9657@gmail.com>
2025-05-19 08:13:45 +0800

    提交 更新接口 和 文章接口

--- big-event/src/main/java/com/cui/controller/CategoryController.java
+++ big-event/src/main/java/com/cui/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.cui.pojo.Result;
 import com.cui.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class CategoryController {
 	private CategoryService categoryService;
 
 	// 新增文章分类
+@PostMapping
 	public Result add(@RequestBody @Validated Category category) {
 		categoryService.add(category);//
 		return Result.success();
