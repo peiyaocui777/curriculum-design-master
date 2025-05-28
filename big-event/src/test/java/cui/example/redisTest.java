c12c68c90f89e944307f0694a42a51be76c59533
xuyucheng <norbocampasjb9657@gmail.com>
2025-05-28 09:02:27 +0800

    修复jar包问题

--- big-event/src/test/java/cui/example/redisTest.java
+++ big-event/src/test/java/cui/example/redisTest.java
@@ -1,7 +1,7 @@
 package cui.example;
 
 import com.cui.BigEventApplication;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
